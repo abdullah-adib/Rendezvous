d0614f7f8855705ae13c1ac92067a577b0b1031c
Aidan Bird <aidan@birdwebwork.com>
2022-05-07 17:32:32 -0400

    added event_requester.py: it caches api results

--- cogs/rdv.py
+++ cogs/rdv.py
@@ -2,7 +2,6 @@ from typing import List
 import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup, ApplicationContext
-# from guilds import DebuggingConstants
 
 class RDV(commands.Cog):
     # Initialize the cog
--- main.py
+++ main.py
@@ -5,32 +5,21 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from cogs.rdv import RDV
+import http.client
 
 # load environment variables from .env
 load_dotenv()
 prefix: str = os.environ.get('PREFIX')
 token: str = os.environ.get('BOT_TOKEN')
+eventToken: str = os.environ.get('TICKETMASTER_TOKEN')
 
 # create bot
 bot = commands.Bot(command_prefix=prefix)
 
-# BASIC COMMANDS
-
-# @bot.command(name = 'ping')
-# async def ping(ctx: commands.Context):
-#     await ctx.reply('Pong!')
-
+# emits a message when the bot is ready
 @bot.event
 async def on_ready():
     print('Bot has started!')
-
-# @bot.event
-# async def on_message(message):
-#     print('f00f')
-#     # if message.content == 'test':
-#     await message.channel.send('Testing 1 2 3')
-#     await bot.process_commands(message)
-
 
 # start bot
 if token is None:
