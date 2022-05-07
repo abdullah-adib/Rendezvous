fe61a3eb0a8996a395c44bbb9de8517217523fed
Aidan Bird <aidan@birdwebwork.com>
2022-05-07 18:48:47 -0400

    global state is now stored in globals.py

--- main.py
+++ main.py
@@ -6,33 +6,36 @@ from cogs.rdv import RDV
 import http.client
 from event_requester import EventRequester
 import os
-import time
+import globals
 
+# set global state
 # load environment variables from .env
 load_dotenv()
-prefix: str = os.environ.get('PREFIX')
-token: str = os.environ.get('BOT_TOKEN')
-eventToken: str = os.environ.get('TICKETMASTER_TOKEN')
+globals.prefix = os.environ.get('PREFIX')
+globals.token = os.environ.get('BOT_TOKEN')
+globals.eventToken = os.environ.get('TICKETMASTER_TOKEN')
+globals.apireq = EventRequester()
+# prefix: str = os.environ.get('PREFIX')
+# token: str = os.environ.get('BOT_TOKEN')
+# eventToken: str = os.environ.get('TICKETMASTER_TOKEN')
 
 # EventRequester example usage
-# ereq = EventRequester()
 # a = ereq.makeTicketMasterAPICall(eventToken, "/discovery/v2/suggest") # default update time is 1 hour
 # a = ereq.makeTicketMasterAPICall(eventToken, "/discovery/v2/suggest") # this will be cached 
 # a = ereq.makeTicketMasterAPICall(eventToken, "/discovery/v2/suggest", 0) # change the update time to 0 seconds
-# time.sleep(2)
 # a = ereq.makeTicketMasterAPICall(eventToken, "/discovery/v2/suggest") # this will be updated
 # exit()
 
 # create bot
-bot = commands.Bot(command_prefix=prefix)
+bot = commands.Bot(command_prefix=globals.prefix)
 
 # emits a message when the bot is ready
 @bot.event
 async def on_ready():
     print('Bot has started!')
 # start bot
-if token is None:
+if globals.token is None:
     print('Bot token is missing! Please check environment variables!')
 else:
     bot.add_cog(RDV(bot))
-    bot.run(token)
+    bot.run(globals.token)
