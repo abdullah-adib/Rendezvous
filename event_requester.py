d179a363e40d7124b15c63d66a711fb5cb7e06c6
Aidan Bird <aidan@birdwebwork.com>
2022-05-07 18:03:54 -0400

    updated event_requester.py

--- event_requester.py
+++ event_requester.py
@@ -1,5 +1,6 @@
-import calendar;
-import time;
+import calendar
+import time
+import http.client
 
 def getUnixTime():
     return calendar.timegm(time.gmtime())
@@ -12,7 +13,7 @@ class EventRequest():
     def __init__(self, url, result, timeCreated, deltaTime):
         self.url = url
         self.result = result
-        self.ageLimit = ageLimit
+        self.timeCreated = timeCreated
         self.deltaTime = deltaTime
 
 class EventRequester:
@@ -23,21 +24,23 @@ class EventRequester:
         res = conn.getresponse()
         return res.read().decode('utf-8')
 
+    def newEventRequest(self, token, requestStr, now, deltaTime):
+        self.requestCache[requestStr] = EventRequest(requestStr, \
+            self.__makeTicketMasterAPICall(token), now, deltaTime)
+
     def makeTicketMasterAPICall(self, token, requestStr, deltaTime = 3600):
         now = getUnixTime()
         ret = None
         # check cache
         if requestStr in self.requestCache:
+            print('value was cached')
             cached = self.requestCache[requestStr]
             if now - cached.timeCreated > cached.deltaTime:
                 # update old cached results
-                self.requestCache[requestStr] = EventRequest(requestStr, \
-                    self.__makeTicketMasterAPICall(token), now, deltaTime)
+                print('cache is old, updating ...')
+                self.newEventRequest(token, requestStr, now, deltaTime)
         else:
-            self.requestCache[requestStr] = EventRequest(requestStr, \
-                    self.__makeTicketMasterAPICall(token), now, deltaTime)
-
-
+            print('value was not cached')
+            self.newEventRequest(token, requestStr, now, deltaTime)
         return self.requestCache[requestStr]
-    ret if ret != None else self.requestCache[requestStr]
 
