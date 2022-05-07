fea689b5c6c13f9bc4a8e8cda0c7594973da7de3
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-05-07 15:40:14 -0400

    Initialize Eventbrite API

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
-# Imports
+#  ================ Imports ==================
 from asyncio.windows_events import NULL
+from eventbrite import Eventbrite
+from discord.ext import commands
 from distutils.log import error
-import os
+from dotenv import load_dotenv
 from unicodedata import name
 import discord
+import os
+# ===========================================
 
-from discord.ext import commands
-from dotenv import load_dotenv
+load_dotenv() # Load the dotenv file
 
-load_dotenv()
-bot = commands.Bot(command_prefix='/')
+api_token: str = os.environ.get('API_TOKEN') # Get the API token from the dotenv file
+eventbrite = Eventbrite(api_token) # Eventbrite API initialization 
 
-@bot.command(name='ping')
-async def ping(ctx: commands.Context):
-    await ctx.reply('Pong!')
+prefix: str = os.environ.get('PREFIX') # Get the prefix from the dotenv file
+bot = commands.Bot(command_prefix=prefix)
 
+# Test command
+# @bot.command(name='test')
+# async def ping(ctx: commands.Context):
+#     await ctx.reply('bot working!')
+
+
+
+# ============= Bot is running =============
 @bot.event
 async def on_ready():
     print('Bot has started!')
 
-token: str = os.environ.get('BOT_TOKEN')
+token: str = os.environ.get('BOT_TOKEN') # Get the bot token and check if it is valid
 if token is None:
     print('Bot token is missing! Please check environment variables!')
 else:
     bot.run(token)
+# ===========================================
