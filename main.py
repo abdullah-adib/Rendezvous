806c92399dd9cfd93cef0282e981ea3f122c8e4d
Caleb Lam <caleb.lam@hotmail.com>
2022-05-07 02:46:24 -0400

    Hide testing commands for now

--- main.py
+++ main.py
@@ -13,21 +13,20 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='/')
 
-@bot.command(name='ping')
-async def ping(ctx: commands.Context):
-    await ctx.reply('Pong!')
+# @bot.command(name='ping')
+# async def ping(ctx: commands.Context):
+#     await ctx.reply('Pong!')
 
 @bot.event
 async def on_ready():
     print('Bot has started!')
 
-@bot.slash_command(name='hi', description='Say hi!', guild_ids=DebuggingConstants.guild_ids)
-async def hi(ctx: ApplicationContext):
-    await ctx.respond('Hi!')
+# @bot.slash_command(name='hi', description='Say hi!', guild_ids=DebuggingConstants.guild_ids)
+# async def hi(ctx: ApplicationContext):
+#     await ctx.respond('Hi!')
 
 token: str = os.environ.get('BOT_TOKEN')
 if token is None:
-    
     print('Bot token is missing! Please check environment variables!')
 else:
     bot.add_cog(RDV(bot=bot))
