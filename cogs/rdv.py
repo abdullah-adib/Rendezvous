4f631b477729d1dfce30b4d820a42d6fcfdf52fc
Aidan Bird <aidan@birdwebwork.com>
2022-05-07 15:09:19 -0400

    fixed help not responding with workaround

--- cogs/rdv.py
+++ cogs/rdv.py
@@ -12,14 +12,29 @@ class RDV(commands.Cog):
         description = 'Rendezvous commands.')
 
     usage = """
-    f
+```
+LIST OF COMMANDS
+1.  /rdv help                     -- Show this help message.
+2.  /rdv [team]                   -- Show events about a sports team.
+3.  /rdv random                   -- Show a random event.
+4.  /rdv price {free/paid}        -- Filter events by price.
+5.  /rdv place {indoors/outdoors} -- Filter events by place.
+6.  /rdv top {int index}          -- Show the events with the most interest.
+7.  /rdv upcoming {int index}     -- Show upcoming events.
+8.  /rdv new                      -- Show new events.
+9.  /rdv {date (mm-dd)}           -- Show events on a particular date.
+10. /rdv sub {event name}         -- subscribe to an event.
+```
     """
 
     # COMMANDS
     # help: DM help to user
-    @rdv.command(description='Shows a description on how to use this bot.')
-    async def help(self, ctx: ApplicationContext):
-        await ctx.interaction.user.send(self.usage)
+    @rdv.command(description='Show the list of commands.')
+    # @rdv.command(description='Shows a description on how to use this bot.')
+    async def help(self, ctx: ApplicationContext, *, message=None):
+        user = ctx.interaction.user
+        await user.send(self.usage)
+        await ctx.respond('DM\'ed {} the command list.'.format(user.mention))
 
     # random
     @rdv.command(description='Fetches a random event.')
